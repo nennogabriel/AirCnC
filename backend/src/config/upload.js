3d83cd062422035d9b921513e0451ab657e4c400
Pedro Nascimento <nennogabriel@gmail.com>
2019-10-03 17:18:00 -0300

    fix:  slugfy

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -6,7 +6,11 @@ module.exports = {
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename: (req, file, cb) => {
       const ext = path.extname(file.originalname);
-      const name = path.basename(file.originalname);
+      const name = path
+        .basename(file.originalname)
+        .toLowerCase() // slugfy
+        .replace(/ /g, '-')
+        .replace(/[^\w-]+/g, '');
 
       cb(null, `${name}-${Date.now()}${ext}`);
     },
