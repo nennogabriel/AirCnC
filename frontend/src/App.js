782d304b6e7624955649ab878d88d73ecc020bbf
Pedro Nascimento <nennogabriel@gmail.com>
2019-10-03 13:34:57 -0300

    frontend: home layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre
+          <strong> talentos </strong> para sua emrpesa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-mail *</label>
+          <input type="email" id="email" placeholder="Seu melhor email" />
+          <button className="btn" type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
