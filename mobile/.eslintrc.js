8f4a56b56c5024ae2ccee3033fbf95eac24a9035
Pedro Nascimento <nennogabriel@gmail.com>
2019-10-03 22:48:44 -0300

    mobile: routes and login page

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
   env: {
     es6: true,
+    jest: true,
   },
   extends: ["airbnb", "prettier", "prettier/react"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    __DEV__: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    __DEV__: true
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
   plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
   rules: {
@@ -21,7 +22,6 @@ module.exports = {
     "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
     "import/prefer-default-export": "off",
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "camelcase": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
   },
