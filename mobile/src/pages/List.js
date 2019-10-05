14e97203729a2bb128ee61c4f70fc6b14cd7719b
Pedro Nascimento <nennogabriel@gmail.com>
2019-10-05 08:03:02 -0300

    mobile: list spots and request visit

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,8 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  AsyncStorage,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  StyleSheet,
+} from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
+  const [techs, setTechs] = useState([]);
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storageTechs => {
+      const techsArray = storageTechs.split(',').map(t => t.trim());
+      setTechs(techsArray);
+    });
+  }, []);
+
   return (
-    <View />
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 10,
+  }
+
+
+});
